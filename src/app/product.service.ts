4466dd4b50f1c4166278a0bea157fd638d34c530
Vinaya <vinaya.godbole@yrcfreight.com>
2018-09-06 15:49:00 -0500

    fix

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
-import { Album } from '/.album';
+import { Album } from './album';
 import {Observable} from 'rxjs/Observable';
 
 @Injectable()
